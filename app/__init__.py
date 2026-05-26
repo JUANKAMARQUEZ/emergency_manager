f70a9b77c86ce7a8ba8a03caa303508b10f7be13
JUANKAMARQUEZ <jcmarquezromero@gmail.com>
2026-05-26 20:26:16 +0200

    modificado el archivo __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -29,4 +29,84 @@ def create_app():
     app.register_blueprint(incident_bp)
     app.register_blueprint(user_bp)
 
+    with app.app_context():
+
+        from .models import (
+            Role,
+            User,
+            IncidentType,
+            Resource
+        )
+
+        from werkzeug.security import generate_password_hash
+
+        # Crear tablas
+        db.create_all()
+
+        # Inicializar solo si está vacío
+        if not Role.query.first():
+
+            # Roles
+            admin_role = Role(name="administrador")
+            user_role = Role(name="usuario")
+
+            db.session.add(admin_role)
+            db.session.add(user_role)
+
+            # Admin
+            admin = User(
+
+                name="Juan Carlos Márquez",
+
+                email="admin@gmail.com",
+
+                password=generate_password_hash(
+                    "admin123"
+                ),
+
+                role=admin_role
+            )
+
+            db.session.add(admin)
+
+            # Tipos incidencia
+            types = [
+
+                "Incendio",
+
+                "Asistencia Sanitaria",
+
+                "Incidencia de Tráfico",
+
+                "Siniestro Vial",
+
+                "Seguridad Ciudadana",
+
+                "Otros"
+            ]
+
+            for t in types:
+                db.session.add(
+                    IncidentType(name=t)
+                )
+
+            # Recursos
+            resources = [
+
+                "A-1", "A-2", "A-3", "A-4", "A-5",
+
+                "P-1", "P-2", "P-3", "P-4", "P-5",
+
+                "B-1", "B-2", "B-3", "B-4", "B-5"
+            ]
+
+            for r in resources:
+                db.session.add(
+                    Resource(name=r)
+                )
+
+            db.session.commit()
+
+            print("Base de datos inicializada")
+
     return app
