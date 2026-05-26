a1f1d26e4f1a6e6b48167b6b3702d7d28c2f10a8
JUANKAMARQUEZ <jcmarquezromero@gmail.com>
2026-05-26 20:41:03 +0200

    modificado el archivo config.py

--- config.py
+++ config.py
@@ -1,7 +1,28 @@
+import os
+
+
 class Config:
 
-    SECRET_KEY = "Kj82nX!pQw93Lm#Tz71AaBcD"
+    SECRET_KEY = os.environ.get(
+        "SECRET_KEY",
+        "Kj82nX!pQw93Lm#Tz71AaBcD"
+    )
 
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost/emergency_db"
+    database_url = os.environ.get(
+        "DATABASE_URL"
+    )
+
+    if database_url:
+
+        database_url = database_url.replace(
+            "postgres://",
+            "postgresql://",
+            1
+        )
+
+    SQLALCHEMY_DATABASE_URI = (
+        database_url
+        or "mysql+pymysql://root:@localhost/emergency_db"
+    )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
