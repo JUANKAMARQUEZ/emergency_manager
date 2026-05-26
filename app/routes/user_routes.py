ff541b21ced0f49ce03bc68dc8880de1b6d0f15a
JUANKAMARQUEZ <jcmarquezromero@gmail.com>
2026-05-26 16:20:18 +0200

    realizada mejoras en gestión de usuarios

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,10 +1,9 @@
 from flask import Blueprint, render_template, request, redirect, flash
 from flask_login import login_required, current_user
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from app.extensions import db
 from app.models import User, Role
-
 
 user_bp = Blueprint("user", __name__)
 
@@ -13,7 +12,8 @@ user_bp = Blueprint("user", __name__)
 # PERFIL
 # -------------------------
 
-@user_bp.route("/profile", methods=["GET","POST"])
+
+@user_bp.route("/profile", methods=["GET", "POST"])
 @login_required
 def profile():
 
@@ -22,35 +22,28 @@ def profile():
     if request.method == "POST":
 
         user.name = request.form["name"]
-        existing_user = User.query.filter_by(
-            email=request.form["email"]
-        ).first()
-
-        if existing_user and existing_user.id != user.id:
-
-            flash(
-                "El email ya está registrado",
-                "danger"
-            )
-
-            return redirect(request.url)
-        user.email = request.form["email"]
-
+       
         # 🔐 cambiar contraseña si se introduce
         if request.form["password"]:
-            # 🔒 validar contraseña
+
+            current_password = request.form["current_password"]
+
+            # 🔒 comprobar contraseña actual
+            if not check_password_hash(user.password, current_password):
+
+                flash("Debes introducir correctamente tu contraseña actual", "danger")
+
+                return redirect(request.url)
+
+            # 🔒 validar nueva contraseña
             if len(request.form["password"]) < 4:
 
-                flash(
-                     "La contraseña debe tener al menos 4 caracteres",
-                    "danger"
-                )
+                flash("La contraseña debe tener al menos 4 caracteres", "danger")
 
                 return redirect(request.url)
 
             user.password = generate_password_hash(
-                request.form["password"],
-                method="pbkdf2:sha256"
+                request.form["password"], method="pbkdf2:sha256"
             )
 
         db.session.commit()
@@ -65,6 +58,7 @@ def profile():
 # -------------------------
 # LISTADO USUARIOS
 # -------------------------
+
 
 @user_bp.route("/users")
 @login_required
@@ -82,7 +76,8 @@ def users():
 # CREAR USUARIO
 # -------------------------
 
-@user_bp.route("/users/create", methods=["GET","POST"])
+
+@user_bp.route("/users/create", methods=["GET", "POST"])
 @login_required
 def create_user():
 
@@ -94,63 +89,46 @@ def create_user():
     if request.method == "POST":
 
         # 📧 comprobar email duplicado
-        existing_user = User.query.filter_by(
-            email=request.form["email"]
-        ).first()
+        existing_user = User.query.filter_by(email=request.form["email"]).first()
 
         if existing_user:
 
-            flash(
-            "El email ya está registrado",
-             "danger"
-             )
+            flash("El email ya está registrado", "danger")
 
             return redirect(request.url)
 
         # 🔒 validar contraseña
         if len(request.form["password"]) < 4:
 
-            flash(
-                "La contraseña debe tener al menos 4 caracteres",
-                "danger"
-            )
+            flash("La contraseña debe tener al menos 4 caracteres", "danger")
 
             return redirect(request.url)
 
         user = User(
-
             name=request.form["name"],
-
             email=request.form["email"],
-
             password=generate_password_hash(
-                request.form["password"],
-                method="pbkdf2:sha256"
+                request.form["password"], method="pbkdf2:sha256"
             ),
-
-            role_id=request.form["role"]
+            role_id=request.form["role"],
         )
 
         db.session.add(user)
         db.session.commit()
 
-        flash(
-            "Usuario creado correctamente",
-            "success"
-        )
+        flash("Usuario creado correctamente", "success")
 
         return redirect("/users")
 
-    return render_template(
-        "user_form.html",
-        roles=roles
-    )
+    return render_template("user_form.html", roles=roles)
+
 
 # -------------------------
 # EDITAR USUARIO
 # -------------------------
 
-@user_bp.route("/users/edit/<int:id>", methods=["GET","POST"])
+
+@user_bp.route("/users/edit/<int:id>", methods=["GET", "POST"])
 @login_required
 def edit_user(id):
 
@@ -162,20 +140,27 @@ def edit_user(id):
     roles = Role.query.all()
 
     if request.method == "POST":
+
+        # 🔒 impedir que el administrador
+        # cambie su propia contraseña aquí
+
+        if int(user.id) == int(current_user.id):
+
+            flash(
+             "  Debes cambiar tu contraseña desde Mi perfil",
+                "warning"
+            )
+
+        return redirect("/profile")
+
         # 📧 comprobar email duplicado
-        existing_user = User.query.filter_by(
-            email=request.form["email"]
-            ).first()
+        existing_user = User.query.filter_by(email=request.form["email"]).first()
 
         if existing_user and existing_user.id != user.id:
 
-            flash(
-                "El email ya está registrado",
-             "danger"
-            )
+            flash("El email ya está registrado", "danger")
             return redirect("/users")
 
-        
         user.name = request.form["name"]
         user.email = request.form["email"]
         user.role_id = request.form["role"]
@@ -183,19 +168,15 @@ def edit_user(id):
         # 🔐 actualizar contraseña si se introduce
         if request.form["password"]:
 
-             # 🔒 validar contraseña
+            # 🔒 validar contraseña
             if len(request.form["password"]) < 4:
 
-                flash(
-                 "La contraseña debe tener al menos 4 caracteres",
-                 "danger"
-                 )
+                flash("La contraseña debe tener al menos 4 caracteres", "danger")
 
-                return redirect(request.url) 
+                return redirect(request.url)
 
             user.password = generate_password_hash(
-                request.form["password"],
-                method="pbkdf2:sha256"
+                request.form["password"], method="pbkdf2:sha256"
             )
 
         db.session.commit()
@@ -204,16 +185,13 @@ def edit_user(id):
 
         return redirect("/users")
 
-    return render_template(
-        "user_form.html",
-        user=user,
-        roles=roles
-    )
+    return render_template("user_form.html", user=user, roles=roles)
 
 
 # -------------------------
 # ELIMINAR USUARIO
 # -------------------------
+
 
 @user_bp.route("/users/delete/<int:id>")
 @login_required
@@ -230,36 +208,25 @@ def delete_user(id):
     # ❌ evitar autoeliminación
     if int(user.id) == int(current_user.id):
 
-        flash(
-            "No puedes eliminar tu propio usuario",
-            "danger"
-        )
+        flash("No puedes eliminar tu propio usuario", "danger")
 
         return redirect("/users")
 
     # ❌ evitar eliminar último administrador
     if user.role.name == "administrador":
 
-        admins = User.query.filter_by(
-            role_id=user.role_id
-        ).all()
+        admins = User.query.filter_by(role_id=user.role_id).all()
 
         if len(admins) <= 1:
 
-            flash(
-                "No puedes eliminar el último administrador",
-                "danger"
-            )
+            flash("No puedes eliminar el último administrador", "danger")
 
             return redirect("/users")
-        
+
     # ❌ evitar eliminar usuarios con incidencias
     if user.incidents:
 
-        flash(
-        "No puedes eliminar un usuario con incidencias asociadas",
-        "danger"
-        )
+        flash("No puedes eliminar un usuario con incidencias asociadas", "danger")
 
         return redirect("/users")
 
