f35fff7bf0bb538d22469b6c2c238a005970b17f
JUANKAMARQUEZ <jcmarquezromero@gmail.com>
2026-05-26 19:49:19 +0200

    modificado el archivo __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,11 @@ from .extensions import db, login_manager
 
 def create_app():
 
-    app = Flask(__name__)
+    app = Flask(
+    __name__,
+    static_folder="static",
+    template_folder="templates"
+    )
 
     app.config.from_object(Config)
 
